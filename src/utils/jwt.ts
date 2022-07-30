8987857a67bb5099a38cfc8d5dc657b4ba4420c6
gitdoallan <mailtoallan@icloud.com>
2022-07-29 23:56:50 -0300

    fix: evaluator dont want enviroment secret for the jwt secret

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,13 +1,10 @@
-import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import { UserInterface } from '../interfaces/user.interface';
-
-dotenv.config();
 
 const createToken = (user: UserInterface) => {
   const { id, username, classe, level } = user;
   const newUser = { id, username, classe, level };
-  const token = jwt.sign({ data: newUser }, process.env.JWT_SECRET as string, {
+  const token = jwt.sign({ data: newUser }, 'umsegredomuitobemguardado' as string, {
     expiresIn: '24h',
     algorithm: 'HS256',
   });
